ce08d42b25c00ba9c4224e2107a5fd85e0b735c6
Zirwa Abid <zirwaabid112@gmail.com>
2025-06-11 00:16:16 +0500

    react

--- RecipePage-React/src/RecipePage.jsx
+++ RecipePage-React/src/RecipePage.jsx
@@ -1,37 +1,130 @@
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
 import CardActionArea from '@mui/material/CardActionArea';
 import Image1 from "./assets/img1.jpeg";
 import "./RecipePage.css"
 
 export default function RecipePage() {
     return <>
-    <div className='card-container'>
-        <Card sx={{ maxWidth: 700, maxHeight: 900 }}>
-            <CardActionArea>
-                <div className="Img">
-                    <CardMedia
-                        component="img"
-                        height="300"
-                        image={Image1}
-                        alt="green iguana"
-                         sx={{ borderRadius: 2 }} 
-                    />
+        <div className='card-container'>
+            <Card className='card' sx={{ maxWidth: 695, Height: 900, borderRadius: 4 }}>
+                <CardActionArea>
+                    <div className="Img">
+                        <CardMedia
+                            component="img"
+                            height="250"
+                            image={Image1}
+                            alt="green iguana"
+                            sx={{ borderRadius: 2 }}
+                        />
 
-                </div>
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="div" >
-                     <h2>  Simple Omelette Recipe</h2>
-                    </Typography>
-                    <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                        Lizards are a widespread group of squamate reptiles, with over 6,000
-                        species, ranging across all continents except Antarctica
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
-        </Card>
-            </div>
+                    </div>
+                    <CardContent>
+
+                        <div className='inner-container'>
+                            <div className='header'>
+                                <p className='header-heading margin'> Simple Omelette Recipe </p>
+
+                                <p className='small-para margin'> An easy and quick dish, perfect for any meal. This classic omelette combines beaten eggs cooked
+                                    to perfection, optionally filled with your choice of cheese, vegetables, or meats.</p>
+                            </div>
+                            <div className='preparation-time'>
+
+                                <span className='margin' style={{ color: "hsl(332, 51%, 32%)", fontWeight: "bolder", fontSize: "1rem", }}>Preparation time
+                                </span>
+                                <p className='small-para margin'>
+                                    <ul>
+                                        <li> <b> Total</b>: Approximately 10 minutes</li>
+                                        <br />
+                                        <li>  <b> Preparation</b>: 5 minutes</li>
+                                        <br />
+                                        <li>  <b> Cooking</b>: 5 minutes</li>
+                                    </ul>
+                                </p>
+                            </div>
+
+                            <div className='ingredients'>
+                                <p className='main-headings'>  Ingredients</p>
+                                <p className='small-para margin'>
+                                    <ul>
+
+                                        <li>2-3 large eggs </li >
+                                        <li>Salt, to taste</li>
+                                        <li> Pepper, to taste</li>
+                                        <li> 1 tablespoon of butter or oil</li>
+                                        <li>Optional fillings: cheese, diced vegetables, cooked meats, herbs</li>
+                                    </ul>
+
+                                </p>
+
+                            </div>
+                            <br />
+                            <hr />
+                            <div className="instructions">
+                                <p className='main-headings'>Instructions</p>
+                                <p className="small-para margin">
+                                    <ol>
+                                        <li>  <b>Beat the eggs</b>: In a bowl, beat the eggs with a pinch of salt and pepper until they are well mixed.
+                                            You can add a tablespoon of water or milk for a fluffier texture.
+                                        </li>
+
+                                        <li>
+                                            <b>Heat the pan</b>: Place a non-stick frying pan over medium heat and add butter or oil.
+                                        </li>
+                                        <li>
+
+                                            <b> Cook the omelette</b>: Once the butter is melted and bubbling, pour in the eggs. Tilt the pan to ensure
+                                            the eggs evenly coat the surface.
+                                        </li>
+                                        <li>
+
+                                            <b> Add fillings (optional)</b>: When the eggs begin to set at the edges but are still slightly runny in the
+                                            middle, sprinkle your chosen fillings over one half of the omelette.
+                                        </li>
+                                        <li>
+
+                                            <b> Fold and serve</b> : As the omelette continues to cook, carefully lift one edge and fold it over the
+                                            fillings. Let it cook for another minute, then slide it onto a plate.
+
+                                        </li>
+                                        <li>
+
+                                            <b>Enjoy</b>: Serve hot, with additional salt and pepper if needed.
+                                        </li>
+                                    </ol>
+
+                                </p>
+                            </div>
+                              <br />
+                            <hr />
+
+                            <div className='nutrition'>
+                                <p className='main-headings'> Nutrition</p>
+
+                                The table below shows nutritional values per serving without the additional fillings.
+
+                                Calories
+                                277kcal
+
+                                Carbs
+                                0g
+
+                                Protein
+                                20g
+
+                                Fat
+                                22g
+
+                            </div>
+                        </div>
+                        <div class="attribution">
+                            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+                            Coded by <a href="#">Your Name Here</a>.
+                        </div>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+        </div >
     </>
 }
