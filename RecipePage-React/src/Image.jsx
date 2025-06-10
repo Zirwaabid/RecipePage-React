54f4fef358bbf433ad502c22b8b7115fd89eb907
Zirwa Abid <zirwaabid112@gmail.com>
2025-06-10 13:42:18 +0500

    react

--- RecipePage-React/src/Image.jsx
+++
@@ -1,8 +0,0 @@
-import RecipeImage from "./assets/image-omelette.jpeg";
-export default function Image() {
-    return <>
-        <img src={RecipeImage} alt="My Asset" />
-    </>
-
-
-}
--- RecipePage-React/src/RecipePage.jsx
+++ RecipePage-React/src/RecipePage.jsx
@@ -1,10 +1,37 @@
 import Card from '@mui/material/Card';
-import Image from './Image';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import CardActionArea from '@mui/material/CardActionArea';
+import Image1 from "./assets/img1.jpeg";
+import "./RecipePage.css"
 
 export default function RecipePage() {
     return <>
-        <Card sx={{ maxWidth: 500,maxHeight:200 }}>
-            <Image />
+    <div className='card-container'>
+        <Card sx={{ maxWidth: 700, maxHeight: 900 }}>
+            <CardActionArea>
+                <div className="Img">
+                    <CardMedia
+                        component="img"
+                        height="300"
+                        image={Image1}
+                        alt="green iguana"
+                         sx={{ borderRadius: 2 }} 
+                    />
+
+                </div>
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div" >
+                     <h2>  Simple Omelette Recipe</h2>
+                    </Typography>
+                    <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                        Lizards are a widespread group of squamate reptiles, with over 6,000
+                        species, ranging across all continents except Antarctica
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
         </Card>
+            </div>
     </>
 }
